b0c434b7131953e502c1ddf5d52be68e5387e127
MahendraAllada <53971938+MahendraAllada@users.noreply.github.com>
2020-04-17 20:42:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "security_key" {
 
 variable "region" {
     description = "EC2 Region for the VPC"
-    default     = "us-east-2"
+    default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
@@ -27,7 +27,7 @@ variable "public_cidr" {
 
 variable "subnet_zone" {
     description = "public availability zone"
-    default     = "us-east-2a"
+    default     = "ap-south-1a"
 }
 
 variable "image" {
@@ -42,5 +42,5 @@ variable "instance_type" {
 
 variable "key" {
     description = "instance key name"
-    default     = "gods"
+    default     = "alladamumbai"
 }
